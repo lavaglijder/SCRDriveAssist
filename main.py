b0fb2bb3def72935ca73a16d528e4aeb344ff4db
lavaglijder <ijsglijder@gmail.com>
2022-08-25 17:32:42 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+VERSION = "0.1-DEV"
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("Starting up drive assist")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
