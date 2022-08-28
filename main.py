603e9496e6efda6b4cfba9983e2bb749c22ed346
lavaglijder <ijsglijder@gmail.com>
2022-08-28 15:56:55 +0200

    fixed input

--- main.py
+++ main.py
@@ -13,16 +13,22 @@ from screengrabber import grab_screen
 
 if __name__ == '__main__':
     print("Starting up drive assist")
-
     train = 'train_' + input("What train are you using? For example 458_0: ")
     while not hasattr(trains, train):
-        train = input("Unknown train name please try again")
+        print(train)
+        train = 'train_' +  input("Unknown train name please try again: ")
     train_info = getattr(trains, train)
 
     route = 'route_' + input("What route are you driving? For example R001: ")
     while not hasattr(routes, route):
-        route = input("Unknown route, please try again")
+        route = 'route_' + input("Unknown route, please try again: ")
     route_info = getattr(routes, route)
+    route_info[0] = str(route_info[0]).strip().replace(" ", "_").lower()
+    route_info[1] = str(route_info[1]).strip().replace(" ", "_").lower()
+
+    bound = input("Are you going north or south bound? Answer either north or south: ")
+    while not bound == 'north' and not bound == 'south':
+        bound = input("Invalid answer: ")
 
     if config.DEBUG:
         stationFile = open("./patterns/stations.txt", "r")
@@ -42,6 +48,10 @@ if __name__ == '__main__':
         start_milliseconds = time.time()
 
         ui = ui_reader(screen)
+        if bound == 'south':
+            ui['destination'] = route_info[1]
+        else:
+            ui['destination'] = route_info[0]
 
         if ui['station_name'] == '':
             print("No fun info skipping")
