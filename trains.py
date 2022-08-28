709c79cb9fd88b2cafa25a781454dc3c8c637d05
lavaglijder <ijsglijder@gmail.com>
2022-08-28 15:35:10 +0200

    Added all routes

--- trains.py
+++ trains.py
@@ -1,4 +1,66 @@
 
 ## (enabled_features, coaches, max_speed)
 ## 1 << 0 -> stop marker detection
+
+#connect
+train_158_0 = (0, 3, 90)
+train_158_9 = (0, 2, 90)
+train_170 = (0, 3, 100)
+train_331_0 = (0, 3, 100)
+train_331_1 = (0, 4, 100)
+train_357 = (0, 4, 100)
+train_380_0 = (0, 3, 100)
+train_380_1 = (0, 4, 100)
+train_444 = (0, 5, 100)
+train_458_0 = (0, 4, 100)
+train_458_5 = (0, 5, 100)
+train_68 = (0, 5, 100)
+
 train_707 = (1 << 0, 5, 100)
+
+train_717 = (0, 6, 85)
+train_730_1 = (0, 5, 100)
+train_195_2 = (0, 4, 100)
+train_168 = (0, 4, 100)
+train_185 = (0, 3, 100)
+train_321 = (0, 4, 100)
+train_333 = (0, 4, 100)
+train_350 = (0, 4, 110)
+train_377 = (0, 5, 100)
+train_465 = (0, 4, 75)
+train_720 = (0, 4, 100)
+
+#express
+train_220 = (0, 4, 125)
+train_221 = (0, 5, 125)
+train_801 = (0, 5, 125)
+train_HST = (0, 6, 125)
+train_HST_5 = (0, 7, 125)
+train_HST_Buffer = (0, 6, 100)
+
+#Waterline
+train_195_0 = (0, 2, 100)
+train_195_1 = (0, 3, 100)
+train_730_0 = (0, 3, 90)
+train_143 = (0, 2, 75)
+train_508 = (0, 3, 75)
+train_165_0 = (0, 2, 90)
+train_166 = (0, 3, 90)
+train_171 = (0, 2, 100)
+train_319 = (0, 4, 100)
+train_379 = (0, 4, 100)
+train_143_double = (0, 4, 100)
+train_165_1 = (0, 4, 100)
+
+#Airlink
+train_185_al = (0, 3, 100)
+train_185_double = (0, 6, 100)
+train_802 = (0, 5, 100)
+train_322 = (0, 4, 100)
+train_360_1 = (0, 4, 100)
+train_755_3 = (0, 3, 100)
+train_387 = (0, 4, 100)
+train_345 = (0, 7, 100)
+train_322_5 = (0, 5, 100)
+train_360_2 = (0, 5, 100)
+train_755_4 = (0, 4, 100)
