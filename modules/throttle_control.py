013bf1c343857021d4bfc38c75f1971ee98a30dc
lavaglijder <ijsglijder@gmail.com>
2022-08-28 16:03:08 +0200

    Fixed throttle

--- modules/throttle_control.py
+++ modules/throttle_control.py
@@ -11,19 +11,24 @@ def update_speed(ui_data, target_speed, max_speed=100):
     if target_speed > max_speed:
         target_speed = max_speed
     target = int(target_speed/max_speed*100)
+    print(target)
     difference_throttle = target - current_throttle
     if -3 < difference_throttle < 0:
         return
     elif difference_throttle > 0:
         if difference_throttle < 5:
-            keyboard.press_and_release("w")
+            keyboard.press("w")
+            time.sleep(0.01)
+            keyboard.release("w")
         else:
             keyboard.press("w")
             time.sleep(difference_throttle * 0.025)
             keyboard.release("w")
     else:
         if difference_throttle > -5:
-            keyboard.press_and_release("s")
+            keyboard.press("s")
+            time.sleep(0.01)
+            keyboard.release("s")
         else:
             keyboard.press("s")
             time.sleep(abs(difference_throttle * 0.025))
